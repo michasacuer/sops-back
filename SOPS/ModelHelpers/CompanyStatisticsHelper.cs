8c9af5902d98b4cb74687c7c32b7aa88377e2e1c
Oskar Dadan <skrzynkanof@gmail.com>
2018-12-26 17:34:34 +0100

    added CompanyStatisticsHelper content

--- SOPS/ModelHelpers/CompanyStatisticsHelper.cs
+++ SOPS/ModelHelpers/CompanyStatisticsHelper.cs
@@ -1,11 +1,35 @@
-﻿using System;
+﻿using SOPS.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
 namespace SOPS.ModelHelpers
 {
-    public class CompanyStatisticsHelper
+    public static class CompanyStatisticsHelper
     {
+        private static DateTime GetCurrentDate()
+        {
+            return DateTime.Now.Date;
+        }
+        public static void IncrementRegistredProducts(this ApplicationDbContext context, int companyId)
+        {
+            var currentDate = GetCurrentDate();
+            var currentStatistics = context.CompanyStatistics.Find(currentDate, companyId);
+            if (currentStatistics == null)
+            {
+                var companyStatistics = new CompanyStatistics()
+                {
+                    Date = currentDate,
+                    CompanyId = companyId,
+                    RegistredProducts = 1
+                };
+                currentStatistics = context.CompanyStatistics.Add(companyStatistics);
+            }
+            else
+            {
+                currentStatistics.RegistredProducts += 1;
+            }
+        }
     }
 }
