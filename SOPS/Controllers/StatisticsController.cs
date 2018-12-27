f6f1005df34ff2aba2bb5321e61243592d4cb6ac
Michał Szewczak <michasacuer3@gmail.com>
2018-12-27 21:48:26 +0100

    Statistic for count of companies and products

---
+++ SOPS/Controllers/StatisticsController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using SOPS.Models;
+
+namespace SOPS.Controllers
+{
+    [RoutePrefix("api/Statistics")]
+    public class StatisticsController : ApiController
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        [HttpGet]
+        [Route("getallcount")]
+        [ResponseType(typeof(StatisticGetAllCountViewModel))]
+        public IHttpActionResult GetAllCount()
+        {
+            return Ok(new StatisticGetAllCountViewModel
+            {
+                CompaniesCount = db.Companies.Count(),
+                AllProductsCount = db.Products.Count()
+            });
+        }
+    }
+}
---
+++ SOPS/Models/StatisticGetAllCountViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SOPS.Models
+{
+    public class StatisticGetAllCountViewModel
+    {
+        public int CompaniesCount { get; set; }
+        public int AllProductsCount { get; set; }
+    }
+}
